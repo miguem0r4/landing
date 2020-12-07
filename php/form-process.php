5b69b60a489c68ef6eab4c880e4b2cca29dcdc06
miguel <ingmiguelmora@hotmail.com>
2020-12-07 18:38:39 -0500

    test email

--- php/form-process.php
+++ php/form-process.php
@@ -12,7 +12,7 @@ require '../vendor/autoload.php';
 
 
 $errorMSG = "";
-$from = "kpzei42za9zs@sportsport.com.co";
+$from = "miguel041106@gmail.com";
 
 
 
@@ -60,20 +60,47 @@ if ($errorMSG == ""){
 //    $success = $mail. mail($EmailTo, $Subject, $Body,$headers);            
 try {  
     $mail = new PHPMailer();
-    $mail->IsSMTP();-
-    $mail->SMTPDebug  = 3;
-    $mail->From = $from;
-    $mail->FromName = "Test";
-    $mail->Host = "localhost"; 
-    $mail->SMTPAuth = false; 
-    $mail->SMTPSecure = false;
-    $mail->SMTPAutoTLS = false;
-    $mail->IsHTML(true);
+    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail­->SMTPAuth = true;
+    $mail­->SMTPSecure = "ssl";
+
+    $mail->Username   = $from;                     // SMTP username
+    $mail->Password   = 'T41esnacionalg';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+/*
+    $mail­->isSMTP();
+//permite modo debug para ver mensajes de las cosas que van ocurriendo
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//Debo de hacer autenticación SMTP
+$mail­->SMTPAuth = true;
+$mail­->SMTPSecure = "ssl";
+//indico el servidor de Gmail para SMTP
+$mail­->Host = "smtp.gmail.com";
+//indico el puerto que usa Gmail
+$mail->Port       = 465;
+//indico un usuario / clave de un usuario de gmail
+$mail­->Username = "miguel041106@gmail.com";
+$mail­->Password = "T41esnacionalg";
+
+  */
+    //Recipients
+    $mail->setFrom($from, 'Miguel angel mora miranda');
     $mail->addAddress($to, 'Miguel Mora');
+    
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = $Subject;
     $mail->Body = $Body;
     $exito = $mail->Send();
+
     echo "Mensaje envíado, pronto nos pondremos en contacto";
+    echo $exito;
       //code...
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
