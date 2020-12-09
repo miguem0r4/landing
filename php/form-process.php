f397d1db9d10854b219962bdb099546fe5079f51
miguel <ingmiguelmora@hotmail.com>
2020-12-09 16:37:04 -0500

    revert

--- php/form-process.php
+++ php/form-process.php
@@ -39,37 +39,31 @@ $EmailTo = "ingmiguelmora@hotmail.com";
 $Subject = "Nuevo mensaje de contacto";
 
 // prepare email body text
-$body = "";
-$logo = '../assets/img/logo.png';
-$link = '#';
-
-$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-$body .= "<table style='width: 100%;'>";
-$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-$body .= "</td></tr></thead><tbody><tr>";
-$body .= "<td style='border:none;'><strong>Nombre:</strong> {$name}</td>";
-$body .= "<td style='border:none;'><strong>Email:</strong> {$email}</td>";
-$body .= "</tr>";	
-$body .= "<tr><td></td></tr>";
-$body .= "<tr><td colspan='2' style='border:none;'>{$message}</td></tr>";
-$body .= "</tbody></table>";
-$body .= "</body></html>";
+$Body = "";
+$Body .= "Nombre: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Mensaje: ";
+$Body .= $message;
+$Body .= "\n";
 
 if ($errorMSG == ""){
 	// send email
     $to = "ingmiguelmora@hotmail.com";
     $headers = "From:" . $from;       
-//    $success = $mail. mail($EmailTo, $Subject, $body,$headers);            
+//    $success = $mail. mail($EmailTo, $Subject, $Body,$headers);            
 try {  
     $mail = new PHPMailer();
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output    
     $mail->isSMTP();                                            // Send using SMTP
     $mail­->SMTPSecure = "ssl";
     $mail->Username   = $from;                     // SMTP username
     $mail->Password   = 'contacto2020';                               // SMTP password
 
     /* Local
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output        
     $mail->SMTPSecure = 'tls';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
@@ -91,7 +85,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = $Subject;
-    $mail->Body = $body;
+    $mail->Body = $Body;
     $exito = $mail->Send();
     echo "Mensaje envíado, pronto nos pondremos en contacto";    
 } catch (Exception $e) {
