5706ef66e4dcb30a35aa0ba37d475928b87ecce2
miguel <ingmiguelmora@hotmail.com>
2020-12-09 12:23:28 -0500

    test email zoho

--- php/form-process.php
+++ php/form-process.php
@@ -61,13 +61,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//    $mail­->SMTPSecure = "ssl";
-    $mail->SMTPSecure = 'tls';
+    $mail­->SMTPSecure = "ssl";
+//    $mail->SMTPSecure = 'tls';
 
     $mail->Username   = $from;                     // SMTP username
     $mail->Password   = 'contacto2020';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 /*
 
@@ -101,7 +101,7 @@ $mail­->Password = "T41esnacionalg";
 
   */
     //Recipients
-    $mail->setFrom($from, 'Miguel angel mora miranda');
+    $mail->setFrom($from, 'contacto');
     $mail->addAddress($to, 'Miguel Mora');
     //$mail->addAddress('contacto@sportspot.com.co', 'Contacto');
     //$mail->addAddress('aumana@sportspot.com.co', 'Andres');
