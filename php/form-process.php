4972aaa985e0233cad2e2608627a71c87fe4efac
miguel <ingmiguelmora@hotmail.com>
2020-12-09 13:07:10 -0500

    test email zoho

--- php/form-process.php
+++ php/form-process.php
@@ -59,69 +59,38 @@ try {
     $mail = new PHPMailer();
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output    
     $mail->isSMTP();                                            // Send using SMTP
-//    $mail->SMTPAuth = true;
-//    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
-//    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail­->SMTPSecure = "ssl";
-//    $mail->SMTPSecure = 'tls';
-
     $mail->Username   = $from;                     // SMTP username
     $mail->Password   = 'contacto2020';                               // SMTP password
-//    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-//    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+    /* Local
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPAuth = true;
+    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+*/
     $mail->Host = 'localhost';
     $mail->SMTPAuth = false;
     $mail->SMTPAutoTLS = false; 
     $mail->Port = 25; 
-/*
-
-$mail = new PHPMailer(true);
-$mail->IsSMTP(); // Using SMTP.
-$mail->CharSet = 'utf-8';
-$mail->SMTPDebug = 2; // Enables SMTP debug information - SHOULD NOT be active on production servers!
-$mail->SMTPAuth = false; // Enables SMTP authentication.
-$mail->Host = "relay-hosting.secureserver.net"; // SMTP server host.
-
-$mail->AddReplyTo('ingmiguelmora@hotmail.com', 'Miguel');
-$mail->SetFrom('contacto@sportspot.com', 'Contacto');
-$mail->Subject = 'PHPMailer Test Subject via smtp, basic with authentication';
-$mail->AltBody = 'To view the message, please use an HTML compatible email viewer!';
-$mail->MsgHTML("Hi, this is an test email");
-*/
-/*
-    $mail­->isSMTP();
-//permite modo debug para ver mensajes de las cosas que van ocurriendo
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-//Debo de hacer autenticación SMTP
-$mail­->SMTPAuth = true;
-$mail­->SMTPSecure = "ssl";
-//indico el servidor de Gmail para SMTP
-$mail­->Host = "smtp.gmail.com";
-//indico el puerto que usa Gmail
-$mail->Port       = 465;
-//indico un usuario / clave de un usuario de gmail
-$mail­->Username = "miguel041106@gmail.com";
-$mail­->Password = "T41esnacionalg";
-
-  */
+  
     //Recipients
     $mail->setFrom($from, 'Contacto SportSpot');
     $mail->addAddress($to, 'Miguel Mora');
-    //$mail->addAddress('contacto@sportspot.com.co', 'Contacto');
-    //$mail->addAddress('aumana@sportspot.com.co', 'Andres');
+    $mail->addAddress('contacto@sportspot.com.co', 'Contacto');
+    $mail->addAddress('aumana@sportspot.com.co', 'Andres');
     
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = $Subject;
     $mail->Body = $Body;
     $exito = $mail->Send();
-
     echo "Mensaje envíado, pronto nos pondremos en contacto";
     echo $exito;
-      //code...
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "El mensaje no pudo ser enviado: {$mail->ErrorInfo}";
 }
 }else{
     if($errorMSG == ""){
