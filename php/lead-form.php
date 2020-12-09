2251eeb5f33c73ea85dfe26bd317ad719b22076b
miguel <ingmiguelmora@hotmail.com>
2020-12-09 13:19:58 -0500

    test email zoho

--- php/lead-form.php
+++ php/lead-form.php
@@ -10,7 +10,7 @@ require '../vendor/autoload.php';
 // Instantiation and passing `true` enables exceptions
 
 $errorMSG = "";
-$from = "miguel041106@gmail.com";
+$from = "contacto@sportspot.com.co";
 
 // EMAIL
 if (empty($_POST["email1"])) {
@@ -34,40 +34,30 @@ if ($errorMSG == ""){
 //    $success = $mail. mail($EmailTo, $Subject, $Body,$headers);            
 try {  
     $mail = new PHPMailer();
-
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output    
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//    $mail­->SMTPSecure = "ssl";
-    $mail->SMTPSecure = 'tls';
-
+    $mail­->SMTPSecure = "ssl";
     $mail->Username   = $from;                     // SMTP username
-    $mail->Password   = 'T41esnacionalg';                               // SMTP password
+    $mail->Password   = 'contacto2020';                               // SMTP password
+
+    /* Local
+    $mail->SMTPSecure = 'tls';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-/*
-    $mail­->isSMTP();
-//permite modo debug para ver mensajes de las cosas que van ocurriendo
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-//Debo de hacer autenticación SMTP
-$mail­->SMTPAuth = true;
-$mail­->SMTPSecure = "ssl";
-//indico el servidor de Gmail para SMTP
-$mail­->Host = "smtp.gmail.com";
-//indico el puerto que usa Gmail
-$mail->Port       = 465;
-//indico un usuario / clave de un usuario de gmail
-$mail­->Username = "miguel041106@gmail.com";
-$mail­->Password = "T41esnacionalg";
-
-  */
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPAuth = true;
+    $mail->Host       = 'smtp.zoho.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+*/
+    $mail->Host = 'localhost';
+    $mail->SMTPAuth = false;
+    $mail->SMTPAutoTLS = false; 
+    $mail->Port = 25; 
+  
     //Recipients
-    $mail->setFrom($from, 'Miguel angel mora miranda');
+    $mail->setFrom($from, 'Contacto SportSpot');
     $mail->addAddress($to, 'Miguel Mora');
-    //$mail->addAddress('contacto@sportspot.com.co', 'Contacto');
-    //$mail->addAddress('aumana@sportspot.com.co', 'Andres');
+    $mail->addAddress('contacto@sportspot.com.co', 'Contacto');
+    $mail->addAddress('aumana@sportspot.com.co', 'Andres');
     
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
@@ -79,7 +69,7 @@ $mail­->Password = "T41esnacionalg";
     echo $exito;
       //code...
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "El mensaje no pudo ser enviado: {$mail->ErrorInfo}";
 }
 }else{
     if($errorMSG == ""){
