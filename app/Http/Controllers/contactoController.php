fcd5ace873b246e456cdba848b8ac0398b6c9537
miguel <ingmiguelmora@hotmail.com>
2020-12-10 19:24:52 -0500

    test email

--- app/Http/Controllers/contactoController.php
+++ app/Http/Controllers/contactoController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class contactoController extends Controller
 {
     function crear(){
-        //return "Mensaje envíado, pronto nos pondremos en contacto";
+        
             
         $data = array('name'=>"Our Code World");
         // Ruta o nombre de la plantilla de hoja que se va a representar
@@ -16,10 +16,11 @@ class contactoController extends Controller
         
         Mail::send(['text'=> $template_path ], $data, function($message) {
                     // Configure el destinatario y el asunto del correo.
-                    $message->to('ingmiguelmora@hotmail.com', 'Miguel Mora')->subject('Laravel First Mail');
+                    $message->to('ingmiguelmora@hotmail.com', 'Miguel Mora')->subject('Nuevo contacto SportSpot');
                     $message->from('contacto@sportspot.com.co','Contacto SportSpot');
-                });        
-        return "Correo electrónico enviado, revise su bandeja de entrada.";
+                });                
+
+        return "Mensaje envíado, pronto nos pondremos en contacto";
 
     }
 
